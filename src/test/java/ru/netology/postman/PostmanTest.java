c0d474ba8f2371953f0f4b2e464360659990f4d3
Alexandra Zhukova <alexandrazhukova2010@mail.ru>
2023-12-01 23:31:19 +0300

    test body

---
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -0,0 +1,23 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+public class PostmanTest {
+    @Test
+    void examination() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+        ;
+    }
+
+}
